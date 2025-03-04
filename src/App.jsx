9c74c91361f4c3be05f828bb8db1067d9f605382
Spandan Hota <spandanhota2005@outlook.com>
2025-03-05 00:43:32 +0530

    add custom cursor and trails

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/navbar/Nav";
-import Home from "./Home";
-import Events from "./events";
+document.addEventListener('mousemove', function (e) {
+  const sparkle = document.createElement('div');
+  sparkle.className = 'sparkle trail';
+  sparkle.style.left = `${e.pageX}px`;
+  sparkle.style.top = `${e.pageY}px`;
+  document.body.appendChild(sparkle);
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-      </Routes>
-    </Router>
-  );
-};
+  setTimeout(() => {
+    sparkle.remove();
+  }, 500);
+});
 
-export default App;
+document.addEventListener('click', function (e) {
+  const sparkle = document.createElement('div');
+  sparkle.className = 'sparkle click';
+  sparkle.style.left = `${e.pageX}px`;
+  sparkle.style.top = `${e.pageY}px`;
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => {
+    sparkle.remove();
+  }, 1000);
+});
