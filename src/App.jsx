f2a4f89f775a8f94e5b054379818c44ff5a29d84
Spandan Hota <spandanhota2005@outlook.com>
2025-03-05 00:54:05 +0530

    fixed sparkle bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-document.addEventListener('mousemove', function (e) {
-  const sparkle = document.createElement('div');
-  sparkle.className = 'sparkle trail';
-  sparkle.style.left = `${e.pageX}px`;
-  sparkle.style.top = `${e.pageY}px`;
-  document.body.appendChild(sparkle);
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Events from "./events";
+import "./sparkles.js";
+import "./cursor.css";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
       </Routes>
     </Router>
   );
 };
-  setTimeout(() => {
-    sparkle.remove();
-  }, 500);
-});
 
-document.addEventListener('click', function (e) {
-  const sparkle = document.createElement('div');
-  sparkle.className = 'sparkle click';
-  sparkle.style.left = `${e.pageX}px`;
-  sparkle.style.top = `${e.pageY}px`;
-  document.body.appendChild(sparkle);
-
-  setTimeout(() => {
-    sparkle.remove();
-  }, 1000);
-});
+export default App;
