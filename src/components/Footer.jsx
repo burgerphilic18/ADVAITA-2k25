6097664438fe1fea176f48728b723200161dd54e
Aman Raj <raj.aman4001@gmail.com>
2025-03-10 00:56:27 +0530

    fixes2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 // Footer.jsx
 import React, { useState, useRef } from 'react';
 import emailjs from '@emailjs/browser';
-import AdvaitaWhite from "/advaita-white.svg"
+import AdvaitaWhite from "../assets/advaita-white.svg"
 import './FooterStyles.css';
 
 const Footer = () => {
@@ -57,7 +57,8 @@ const Footer = () => {
 
         <div className="logo-section">
           <div className="foot-links pt-serif-regular">
-            <a href="/home">Home</a>
+            <a href="/Home">Home</a>
+            
             <a href="https://konfhub.com/a02e541d-da87-465b-ad6d-d61ca8384b75" target="_blank" rel="noopener noreferrer">Passes</a>
             <a href="#" target="_blank" rel="noopener noreferrer">Accomodation</a>
             <a href="https://unstop.com/f/PsILQcN?utm_medium=Share&utm_source=shortUrl " target="_blank" rel="noopener noreferrer">Register</a>
@@ -78,14 +79,14 @@ const Footer = () => {
       </div>
       
      
-      <div className="footer-right ">
+      <div className="footer-right">
         <div className='contact-form-foot pt-serif-regular'>
         <h2 className="contact-heading harry-potter-f">CONTACT US</h2>
         
         <form ref={form} onSubmit={handleSubmit} className="contact-form">
           <div className="form-row">
             <div className="form-group">
-              <input
+              <input 
                 type="text"
                 name="name"
                 placeholder="Name"
@@ -146,11 +147,9 @@ const Footer = () => {
         </form>
         
         </div>
-        <div className="social-links">
+        <div className="social-links pt-serif-regular">
           <p className="join-text">Join Us</p>
-          <a href="https://in.linkedin.com/school/international-institute-of-information-technology-bhubaneswar/" target="_blank" rel="noopener noreferrer" className="social-icon"/>
-          <p className="join-text pt-serif-regular">Join Us</p>
-          <a href="https://in.linkedin.com/school/international-institute-of-information-technology-bhubaneswar/" className="social-icon">
+          <a href="https://in.linkedin.com/school/international-institute-of-information-technology-bhubaneswar/" target="_blank" rel="noopener noreferrer" className="social-icon">
             <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 24 24">
               <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
             </svg>
@@ -165,7 +164,6 @@ const Footer = () => {
               <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
             </svg>
           </a>
-
           <a href="mailto:advaita@iiit-bh.ac.in" target="_blank" rel="noopener noreferrer" className="social-icon">
             <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 24 24">
               <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/>
