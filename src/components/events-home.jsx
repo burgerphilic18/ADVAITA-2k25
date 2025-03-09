c4989b2db7c90ae96c1890b30a9987d3033ac3a8
Spandan Hota <spandanhota2005@outlook.com>
2025-03-10 00:28:43 +0530

    update social icons

--- src/components/events-home.jsx
+++ src/components/events-home.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import './events-home.css';
 import Navbar from "../components/navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
+
 const EventsHome = () => {
   return (
     <div className="events-home">
@@ -15,10 +18,16 @@ const EventsHome = () => {
             <a className="download-button" href=" " target="_blank" rel="noreferrer">Download Rulebook</a>
         </div>
         <div className="leftVertical">
-                    <div>FOLLOW US ON: </div>
-                    <a rel="noreferrer" target="_blank" href="https://instagram.com/advaita_iiitbh"><img src="" alt="social" /></a>
-                    <a rel="noreferrer" target="_blank" href="https://facebook.com/advaita.iiit"><img src="" alt="social" /></a>
-                    <a rel="noreferrer" target="_blank" href="https://youtube.com/channel/UCkULTsUj0_Ukm9DWOtin5yw"><img src="" alt="social" /></a>
+        <div>FOLLOW US ON: </div>
+            <a rel="noreferrer" target="_blank" href="https://instagram.com/advaita_iiitbh">
+              <FontAwesomeIcon icon={faInstagram} style={{ color: "#f1ce89" }} className="social-icons"/>
+            </a>
+            <a rel="noreferrer" target="_blank" href="https://facebook.com/advaita.iiit">
+              <FontAwesomeIcon icon={faFacebook} style={{ color: "#f1ce89" }} className="social-icons"/>
+            </a>
+            <a rel="noreferrer" target="_blank" href="https://www.youtube.com/@AdvaitaIIITBhubaneswar">
+              <FontAwesomeIcon icon={faYoutube} style={{ color: "#f1ce89" }} className="social-icons"/>
+            </a>
       </div>
       <div className="rightVertical">
           IIIT BHUBANESWAR, ODISHA
