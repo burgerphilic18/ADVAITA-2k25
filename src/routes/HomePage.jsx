b5d37a5bbce1abc8631da3ea57a4e2adf936af19
Spandan Hota <spandanhota2005@outlook.com>
2025-03-09 20:05:30 +0530

    homepage changes

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,22 +1,30 @@
 import React from "react";
 import "./HomePageStyle.css"; 
 import MainLogo from "../assets/Main-logo.svg";
-import DateBanner from "../components/dateBanner/dateBanner";
 import Navbar from "../components/navbar/Nav";
 
 const HomePage = () => {
   return (
     <div id="homepage">
+      <div className="leftVertical">
+                    <div>FOLLOW US ON: </div>
+                    <a rel="noreferrer" target="_blank" href="https://instagram.com/advaita_iiitbh"><img src="" alt="social" /></a>
+                    <a rel="noreferrer" target="_blank" href="https://facebook.com/advaita.iiit"><img src="" alt="social" /></a>
+                    <a rel="noreferrer" target="_blank" href="https://youtube.com/channel/UCkULTsUj0_Ukm9DWOtin5yw"><img src="" alt="social" /></a>
+      </div>
+      <div className="rightVertical">
+          IIIT BHUBANESWAR, ODISHA
+      </div>
       <Navbar />
       <div className="MainLogoDiv">
         <img src={MainLogo} alt="Advaita" />
         
       </div>
-      < DateBanner/>
+      <div className="dateBanner">
+        <img src="/dateBanner.webp" alt="dateBanner" />
+      </div>     
 
-      <div className="vertical-iiit-text">
-        IIIT BHUBANESWAR
-      </div>
+      
       
     </div>
   );
