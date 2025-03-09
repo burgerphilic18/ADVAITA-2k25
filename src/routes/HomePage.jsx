9f8ac243da1f8b5fc22e8e7d9ff50ab29ded58ab
Spandan Hota <70068606+burgerphilic18@users.noreply.github.com>
2025-03-10 01:36:49 +0530

    Revert "update social icons"

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -2,24 +2,15 @@ import React from "react";
 import "./HomePageStyle.css"; 
 import MainLogo from "../assets/Main-logo.svg";
 import Navbar from "../components/navbar/Nav";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faFacebook, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const HomePage = () => {
   return (
     <div id="homepage">
-      <div className="leftVertical">
-      <div>FOLLOW US ON: </div>
-        <a rel="noreferrer" target="_blank" href="https://instagram.com/advaita_iiitbh">
-          <FontAwesomeIcon icon={faInstagram} style={{ color: "#f1ce89" }} className="social-icons"/>
-        </a>
-        <a rel="noreferrer" target="_blank" href="https://facebook.com/advaita.iiit">
-          <FontAwesomeIcon icon={faFacebook} style={{ color: "#f1ce89" }} className="social-icons"/>
-        </a>
-        <a rel="noreferrer" target="_blank" href="https://www.youtube.com/@AdvaitaIIITBhubaneswar">
-          <FontAwesomeIcon icon={faYoutube} style={{ color: "#f1ce89" }} className="social-icons"/>
-        </a>
-
+      <div className="leftVertical pt-serif-regular">
+                    <div>FOLLOW US ON: </div>
+                    <a rel="noreferrer" target="_blank" href="https://instagram.com/advaita_iiitbh"><img src="" alt="social" /></a>
+                    <a rel="noreferrer" target="_blank" href="https://facebook.com/advaita.iiit"><img src="" alt="social" /></a>
+                    <a rel="noreferrer" target="_blank" href="https://youtube.com/channel/UCkULTsUj0_Ukm9DWOtin5yw"><img src="" alt="social" /></a>
       </div>
       <div className="rightVertical pt-serif-regular">
           IIIT BHUBANESWAR, ODISHA
