a9be06d9343a86a5b4e552a75127e9ac0dfca29a
Spandan Hota <spandanhota2005@outlook.com>
2025-03-10 13:54:28 +0530

    links, images, bg adjusted

--- src/components/gryffindor-schedule.jsx
+++ src/components/gryffindor-schedule.jsx
@@ -9,7 +9,7 @@ const Gryffindor_Schedule = () => {
           venue: "PLACEMENT CELL",
           date: "29 March 2025",
           time: "09:00 pm - Overnight",
-          image: "/sample-image.png" ,
+          image: "/hackathon.webp" ,
           description: 'Join "Hackfest-Advaita," the ultimate inter-college hackathon! Students unite, sharing knowledge and collaborating on groundbreaking projects in the realm of technology. Seize the opportunity to innovate and connect with like-minded minds!',
           link: 'https://unstop.com/p/hackfest-international-institute-of-information-technology-iiit-bhubaneswar-1413902'
         },
@@ -19,7 +19,7 @@ const Gryffindor_Schedule = () => {
           venue: "PLACEMENT CELL",
           date: "29 March 2025",
           time: "09:00 pm - Overnight",
-          image: "/sample-image.png" ,
+          image: "/ctf.webp" ,
           description: 'Embark on a cybersecurity adventure! Compete in challenges to capture hidden "flags" within the dynamic game environment. Rule variations add intrigue to each Flag. ',
           link: 'https://unstop.com/p/inter-college-ctf-event-advaita-annual-techno-cultural-fest-international-institute-of-information-technology-iiit-bhu-1413963'
         },
@@ -29,7 +29,7 @@ const Gryffindor_Schedule = () => {
             venue: "CG05-CG08",
             date: "29 March 2025",
             time: "03:00 pm - 06:00pm",
-            image: "/sample-image.png" ,
+            image: "/cf.webp" ,
             description: "Join our exhilarating team-based programming competition! Test your adaptability and coordination as you and your partner tackle distinct problem sets in separate rooms. Switch places at regular intervals, maneuvering each other's code to secure those coveted green ticks. ",
             link: 'https://unstop.com/p/code-fiesta-advaita-annual-techno-cultural-fest-international-institute-of-information-technology-iiit-bhubaneswar-1413956'
         },
@@ -39,7 +39,7 @@ const Gryffindor_Schedule = () => {
             venue: "CG02",
             date: "30 March 2025",
             time: "10:00 am - 3:00pm",
-            image: "/sample-image.png",
+            image: "/tech-talk.webp",
             description: 'Tech talks are discussions about technology and communication, and can be educational, inspirational, or motivational. They can take place in companies, at conferences, or online. ' ,
             link: 'https://unstop.com/conferences/beyond-boundaries-tech-talk-advaita-annual-techno-cultural-fest-international-institute-of-information-techn-1413944'
         },
@@ -51,7 +51,7 @@ const Gryffindor_Schedule = () => {
             time: "03:00 pm - 6:00pm",
             description: "It's football time and no one plays soccer better than the engineers, in their sophisticated way we hail this event ROBO SOCCER. So if you are up for the challenge get your manual soccer-playing bots ready to compete on the biggest stage ",
             link: 'https://unstop.com/p/roborogue-advaita-annual-techno-cultural-fest-international-institute-of-information-technology-iiit-bhubaneswar-1413857',
-            image: "/sample-image.png" 
+            image: "/robosoccer.webp" 
         },
         {
             id: 6,
@@ -59,7 +59,7 @@ const Gryffindor_Schedule = () => {
             venue: "OPEN SPACE",
             date: "28 March 2025",
             time: "03:00 pm - 6:00pm",
-            image: "/sample-image.png",
+            image: "/robosumo.webp",
             description: "Dive into the ultimate off-road challenge with your bots! Conquer rugged terrains, unleash adrenaline, and chase victory. Join now for an unforgettable thrill!",
             link: 'https://unstop.com/p/brawlbounty-advaita-annual-techno-cultural-fest-international-institute-of-information-technology-iiit-bhubaneswar-1413218'
         },
@@ -69,7 +69,7 @@ const Gryffindor_Schedule = () => {
             venue: "CG02",
             date: "27-30 March 2025",
             time: "Open All Time",
-            image: "/sample-image.png" ,
+            image: "/expo.webp" ,
             description: " Invento Expo is an exciting robotics exhibition held as part of our annual fest. It serves as a platform for students, tech enthusiasts, and innovators to showcase cutting-edge robotic projects, automation solutions, and AI-driven prototypes",
             link: 'https://unstop.com/p/invento-expo-advaita-annual-techno-cultural-fest-international-institute-of-information-technology-iiit-bhubaneswar-1413166'
         },
@@ -79,7 +79,7 @@ const Gryffindor_Schedule = () => {
             venue: "CG07",
             date: "29 March 2025",
             time: "11:00 am -12:00pm",
-            image: "/sample-image.png",
+            image: "/ensemble.png",
             description: " From designing innovative circuits to solving real-world electrical challenges, Ensemble tests both creativity and technical expertise. Whether you're a beginner or a pro, Circuit Design offers a hands-on experience in analog, digital, and embedded systems. Get ready to build, innovate, and compete!",
             link: "" 
         },
